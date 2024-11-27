a83008f9859ec9517b5e070fbce85744700e7c44
nightFuryCodes <ashwinkharkwal007@gmail.com>
2024-11-26 21:38:37 -0500

    highScore

--- snake_game/main.py
+++ snake_game/main.py
@@ -35,16 +35,16 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.food_move()
         snake.increase_snake()
-        scoreboard.new_score()
+        scoreboard.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        snake.reset()
+        scoreboard.reset()
 
-    for segment in snake.segments[1:]:
-        
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
 
 screen.exitonclick()
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -7,19 +7,27 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.penup()
+        self.high_score = 0
         self.goto(0, 270)
         self.color("white")
-        self.write(f"Score: {self.score}", align='center', font=('Arial', 24, 'normal'))
+        self.write(f"Score: {self.score}, High Score: {self.high_score}", align='center', font=('Arial', 24, 'normal'))
         self.hideturtle()
 
     def new_score(self):
-        self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align='center', font=('Arial', 24, 'normal'))
+        self.write(f"Score: {self.score}, High Score: {self.high_score}", align='center', font=('Arial', 24, 'normal'))
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"Game Over", align='center', font=('Arial', 24, 'normal'))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.new_score()
+
+
+    def increase_score(self):
+        self.score += 1
+        self.new_score()
 
 
 
